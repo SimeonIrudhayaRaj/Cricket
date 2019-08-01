cb1589bab70ef13114bf68f5d204babc0f9052b1
SIME0N <irudhayaraj.webmaster@gmail.com>
2019-08-01 22:59:33 +0530

    swing animation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var batLeft;
 var release,pitch,hit;
 onmousemove = function(e)
 {
-    console.log("mouse location:", e.clientX, e.clientY)
+    // console.log("mouse location:", e.clientX, e.clientY)
    
     //   if(e.clientY>200&&e.clientY<340&&e.clientX>90&&e.clientX<500)
     {
@@ -19,11 +19,20 @@ onmousemove = function(e)
 
 }   
 bowl= setInterval(newBall,3000)
-stumpHit= setInterval(stCollide,50)
-batHit = setInterval(collide,50);
-
+stumpHit= setInterval(stCollide,1)
+batHit = setInterval(collide,1);
+$('#bat').animate(
+    { deg: 45},
+    {
+      duration: 150,
+      step: function(now) {
+        $(this).css({ transform: 'rotate(' + now + 'deg)' });
+      }
+    }
+  );
 function stCollide()
 {
+    var rot=0
     var ballPos= $("#ball").position();
     var ballTop=ballPos.top;
     var ballLeft=ballPos.left;
@@ -32,9 +41,11 @@ function stCollide()
     var stLeft=stPos.left;
     if(ballLeft>=stLeft-40&&ballLeft<=stLeft+25&&ballTop>=stTop-20&&ballTop<=stTop+320)
     {
+        clearInterval(stumpHit)
+
         alert("out\n Your score :"+score);
         score=0
-        run=0;
+        run=0
         $("#score").text(score);
         $("#bat").css("left",100);
         $("#bat").css("top",300);
@@ -45,12 +56,14 @@ function collide()
     var ballPos= $("#ball").position();
     var ballTop=ballPos.top;
     var ballLeft=ballPos.left;
-    console.log("ball location:", ballLeft, ballTop)
+    // console.log("ball location:", ballLeft, ballTop)
 
     if(ballLeft>=batLeft+10&&ballLeft<=batLeft+60&&ballTop>=batTop&&ballTop<=batTop+290)
     {   
-        
-            $("#ball").stop(true,false);
+        console.log("ball location:", rot)
+
+        clearInterval(batHit)
+        $("#ball").stop(true,false);
         if(ballTop<=batTop+100)
         {
             $("#ball").animate({top: "600px", left:"80px"},800,"linear");
@@ -76,11 +89,14 @@ function collide()
             $("#ball").animate({top: "540px", left:"1600px"},800,"linear");
             run=2;
 
-        }else if(ballTop<=batTop+290)
+        }else if(ballTop<=batTop+280)
         {
             $("#ball").animate({top: "600px", left:"1600px"},800,"linear");
             run=1;
-
+        }
+        else if(ballTop<=batTop+300)
+        {
+            $("#ball").animate({top: "615px", left:"80px"},800,"linear");
         }
         $("#score").text(score);
     }
@@ -88,6 +104,8 @@ function collide()
 
 function newBall()
 {
+    stumpHit= setInterval(stCollide,1)
+    batHit = setInterval(collide,1);        
     release=getRandomArbitrary(50,600);
     pitch=getRandomArbitrary(250,600);
     hit=getRandomArbitrary(200,520);
@@ -102,4 +120,71 @@ function newBall()
 function getRandomArbitrary(min, max)
 {
     return Math.random() * (max - min) + min;
+}
+
+$('#ball').on('change', function () {
+    console.log('css changed');
+});
+$('#ball').bind('style', function() {
+    console.log('css changed');
+});
+
+
+
+function batRotate(){
+    rot=0
+    $('#bat').animate(
+        { deg: 0 },
+        {
+          duration: 50,
+          step: function(now) {
+            $(this).css({ transform: 'rotate(' + now + 'deg)' });
+          },
+          complete: function(){
+            rot=1
+
+          }
+        }
+      );
+      $('#bat').animate(
+        { deg: -45},
+        {
+          duration: 50,
+          step: function(now) {
+            $(this).css({ transform: 'rotate(' + now + 'deg)' });
+          },
+          complete: function(){
+            rot=2
+
+          }
+        }
+      );
+      $('#bat').animate(
+        { deg: 0},
+        {
+          duration: 50,
+          step: function(now) {
+            $(this).css({ transform: 'rotate(' + now + 'deg)' });
+          },
+          complete: function(){
+            rot=1
+
+          }
+        }
+      );
+      $('#bat').animate(
+        { deg: 45},
+        {
+          duration: 50,
+          step: function(now) {
+            $(this).css({ transform: 'rotate(' + now + 'deg)' });
+          }
+          ,
+          complete: function(){
+            rot=0
+
+          } 
+        }
+      );
+        
 }
