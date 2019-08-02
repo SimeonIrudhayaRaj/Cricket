bb8ae2129363a66ee1e79b0f974b914d02e10b15
SIME0N <irudhayaraj.webmaster@gmail.com>
2019-08-03 00:04:25 +0530

    defence

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var score=0,run=0;
 var batTop;
 var batLeft;
 var release,pitch,hit;
+var flag=true
 onmousemove = function(e)
 {
     // console.log("mouse location:", e.clientX, e.clientY)
@@ -12,12 +13,12 @@ onmousemove = function(e)
     batTop=e.clientY
     $("#bat").css("left",e.clientX);
     $("#bat").css("top",e.clientY);
-    collide()
     $("#score").text(score);
 
     }
 
 }   
+
 bowl= setInterval(newBall,3000)
 stumpHit= setInterval(stCollide,1)
 batHit = setInterval(collide,1);
@@ -39,8 +40,9 @@ function stCollide()
     var stPos= $("#stump").position();
     var stTop=stPos.top;
     var stLeft=stPos.left;
-    if(ballLeft>=stLeft-40&&ballLeft<=stLeft+25&&ballTop>=stTop-20&&ballTop<=stTop+320)
+    if(ballLeft>=stLeft-40&&ballLeft<=stLeft+25&&ballTop>=stTop-20&&ballTop<=stTop+320&&flag)
     {
+        flag=true
         clearInterval(stumpHit)
 
         alert("out\n Your score :"+score);
@@ -56,79 +58,154 @@ function collide()
     var ballPos= $("#ball").position();
     var ballTop=ballPos.top;
     var ballLeft=ballPos.left;
+
+    // if(rot==0&&flag)
+    // {
+    //   if(ballLeft>=batLeft+10&&ballLeft<=batLeft+60&&ballTop>=batTop&&ballTop<=batTop+290)
+    //   { 
+
+    //     if(batTop-ballTop>250&&batLeft-ballLeft<50)
+    //     {
+    //       flag=false      
+    //       $("#ball").stop(true,false);
+    //       ballLeft=ballLeft+200;
+    //       $("#ball").animate({top: "600px", left:ballLeft+"px"},200,"linear");
+    //       ballLeft=ballLeft+100;
+    //       $("#ball").animate({top: "550px", left:ballLeft+"px"},200,"linear");
+    //       ballLeft=ballLeft+30;
+    //       $("#ball").animate({top: "550px", left:ballLeft+"px"},50,"linear");
+    //       ballLeft=ballLeft+100;
+    //       $("#ball").animate({top: "600px", left:ballLeft+"px"},200,"linear");
+    //       ballLeft=ballLeft+200;
+    //       $("#ball").animate({top: "600px", left:ballLeft+"px"},400,"linear");
+    //     }
+    //     clearInterval(batHit)
+
+    //   }
+    // }
+
     // console.log("ball location:", ballLeft, ballTop)
+// if(flag)
+// {
+    if(ballLeft>=batLeft-100&&ballLeft<=batLeft+60&&ballTop>=batTop&&ballTop<=batTop+290&&flag)
+    {  
+        // console.log("ball location:", rot)
 
-    if(ballLeft>=batLeft+10&&ballLeft<=batLeft+60&&ballTop>=batTop&&ballTop<=batTop+290)
-    {   
-        console.log("ball location:", rot)
+        if((ballTop<=batTop+130&&ballLeft<batLeft+130))
+        {           
 
-        clearInterval(batHit)
-        $("#ball").stop(true,false);
-        if(ballTop<=batTop+100)
-        {
-            $("#ball").animate({top: "600px", left:"80px"},800,"linear");
-        }
-        else if(ballTop<=batTop+110)
-        {
-            $("#ball").animate({top: "600px", left:"1600px"},800,"linear");
-            run=1;
-
+          flag=false 
+          console.log("bottom1");
+          $("#ball").stop(true,false);
+          ballLeft=ballLeft+200;
+          $("#ball").animate({top: "600px", left:ballLeft+"px"},200,"linear");
+          ballLeft=ballLeft+100;
+          $("#ball").animate({top: "550px", left:ballLeft+"px"},200,"linear");
+          ballLeft=ballLeft+30;
+          $("#ball").animate({top: "550px", left:ballLeft+"px"},50,"linear");
+          ballLeft=ballLeft+100;
+          $("#ball").animate({top: "600px", left:ballLeft+"px"},200,"linear");
+          ballLeft=ballLeft+200;
+          $("#ball").animate({top: "600px", left:ballLeft+"px"},400,"linear");
+          run=0
+          if(ballLeft>1480)
+              run=1
         }else if(ballTop<=batTop+150)
-        {
-            $("#ball").animate({top: "30px", left:"1600px"},800,"linear");
-            run=6;
-
-        } else if(ballTop<=batTop+230)
-        {
-            $("#ball").animate({top: "100px", left:"1600px"},800,"linear");
-            run=4;
+        {        flag=false 
+          console.log("bottom3");
+          $("#ball").stop(true,false);
+          if(ballTop>400)
+          {
+            ballLeft=ballLeft+200;
+            $("#ball").animate({top: "600px", left:ballLeft+"px"},200,"linear");
+            ballLeft=ballLeft+100;
+            $("#ball").animate({top: "550px", left:ballLeft+"px"},200,"linear");
+            ballLeft=ballLeft+30;
+            $("#ball").animate({top: "550px", left:ballLeft+"px"},50,"linear");
+            ballLeft=ballLeft+100;
+            $("#ball").animate({top: "600px", left:ballLeft+"px"},200,"linear");
+            ballLeft=ballLeft+200;
+            $("#ball").animate({top: "600px", left:ballLeft+"px"},400,"linear");
+            run=0
+            if(ballLeft>1480)
+              run=1
+          }else
+          {
+            ballLeft=(ballLeft+750)/2;
+            $("#ball").animate({top: "600px", left:ballLeft+"px"},300,"linear");
+            ballLeft=(ballLeft+1250)/2;
+            $("#ball").animate({top: "500px", left:ballLeft+"px"},300,"linear");
+            ballLeft=ballLeft+30;
+            $("#ball").animate({top: "500px", left:ballLeft+"px"},50,"linear");
+            ballLeft=ballLeft+100;
+            $("#ball").animate({top: "600px", left:"1250px"},500,"linear");
+            ballLeft=ballLeft+200;
+            $("#ball").animate({top: "600px", left:"1500px"},600,"linear");
+            run=1
+          }
 
         }
-        else if(ballTop<=batTop+260)
-        {
-            $("#ball").animate({top: "540px", left:"1600px"},800,"linear");
-            run=2;
+        else if((ballTop<=batTop+260&&ballLeft<batLeft-30)||(ballTop<=batTop+230&&ballLeft<batLeft-15)||(ballTop<=batTop+200&&ballLeft<batLeft+5)||(ballTop<=batTop+180&&ballLeft<batLeft+30)||(ballTop<=batTop+155&&ballLeft<batLeft+45))
+        {        flag=false 
+          console.log("bottom5");
+          $("#ball").stop(true,false);
+                ballLeft=ballLeft+200;
+                $("#ball").animate({top: "600px", left:ballLeft+"px"},200,"linear");
+                ballLeft=ballLeft+100;
+                $("#ball").animate({top: "550px", left:ballLeft+"px"},200,"linear");
+                ballLeft=ballLeft+30;
+                $("#ball").animate({top: "550px", left:ballLeft+"px"},50,"linear");
+                ballLeft=ballLeft+100;
+                $("#ball").animate({top: "600px", left:ballLeft+"px"},200,"linear");
+                ballLeft=ballLeft+200;
+                $("#ball").animate({top: "600px", left:ballLeft+"px"},400,"linear");
+                run=0
+                if(ballLeft>1480)
+                  run=1
 
-        }else if(ballTop<=batTop+280)
-        {
-            $("#ball").animate({top: "600px", left:"1600px"},800,"linear");
-            run=1;
-        }
-        else if(ballTop<=batTop+300)
-        {
-            $("#ball").animate({top: "615px", left:"80px"},800,"linear");
         }
         $("#score").text(score);
+        clearInterval(batHit)
+
     }
+//   }
 }
 
 function newBall()
-{
+{ 
+    flag=true
     stumpHit= setInterval(stCollide,1)
     batHit = setInterval(collide,1);        
-    release=getRandomArbitrary(50,600);
-    pitch=getRandomArbitrary(250,600);
-    hit=getRandomArbitrary(200,520);
+    release=getRandomArbitrary(50,300);
+    pitch=getRandomArbitrary(110,600);
+    if(pitch>500)
+    {
+      hit=getRandomArbitrary(150,320);
+    }else if(pitch>400)
+    {
+      hit=getRandomArbitrary(350,420);
+    }else if(pitch>300)
+    {
+      hit=getRandomArbitrary(450,500);
+    }else if(pitch>200)
+    {
+      hit=getRandomArbitrary(500,580);
+    }else{
+      hit=getRandomArbitrary(550,600);
+
+    }
     score=score+run
     $("#score").text(score);
     $("#ball").animate({top: release+"px", left:"1400px"},0,"linear");
     
-    $("#ball").animate({top: "600px", left:pitch+"px"},800,"linear");
-    $("#ball").animate({top: hit+"px", left:"-50px"},500,"linear");
+    $("#ball").animate({top: "600px", left:pitch+"px"},500,"linear");
+    $("#ball").animate({top: hit+"px", left:"-50px"},300,"linear");
 }
    
 function getRandomArbitrary(min, max)
 {
     return Math.random() * (max - min) + min;
 }
-
-$('#ball').on('change', function () {
-    console.log('css changed');
-});
-$('#ball').bind('style', function() {
-    console.log('css changed');
-});
-
 
 
 function batRotate(){
